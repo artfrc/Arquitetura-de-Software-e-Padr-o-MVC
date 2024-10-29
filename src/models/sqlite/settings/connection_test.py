2d65d87523d50bb5ec0fc2ae04adbdebfcdef10c
Arthur <crfernandesarthur@gmail.com>
2024-10-29 10:33:19 -0300

    config implemeting models dir with connection to sqlite

--- src/models/sqlite/settings/connection_test.py
+++ src/models/sqlite/settings/connection_test.py
@@ -1,4 +1,13 @@
-# from .connection import db_connection_handler
+import pytest
+from sqlalchemy.engine import Engine
+from .connection import db_connection_handler
 
+@pytest.mark.skip(reason="interation with data base")
 def test_connect_to_db():
-      pass
+   assert db_connection_handler.get_engine() is None 
+
+   db_connection_handler.connect_to_db()
+   db_engine = db_connection_handler.get_engine()
+   
+   assert db_engine is not None
+   assert isinstance(db_engine, Engine)
